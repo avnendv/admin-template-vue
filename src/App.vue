23e691d8a39745fffee5f1353e7f656acc8b66ce
avnendv <viet.nd.avnendv@gmail.com>
2024-05-08 17:32:45 +0700

    feat: change title on change route

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+watchEffect(() => {
+  if (route.meta?.title)
+    // translate page title
+    document.title = route.meta.title
+})
 </script>
 
 <template>
